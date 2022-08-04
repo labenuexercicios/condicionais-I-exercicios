59e089f7bda0693b0ee5363a8dcd4075bd5e121a
Thaila Marani <thaila_marani@hotmail.com>
2022-08-04 15:21:07 -0300

    Entrega do exercício de prática aula 012

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,13 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = 30
+let isMaiorIdade = true
+let isCursandoOutraFaculdade = true
 
-if(){
-
-}else{
-	
-}
+if(idade >=18)
+{console.log('Pessoa maior de idade.')}
+else console.log('Pessoa menor de idade');
+if (isMaiorIdade === true)
+{console.log('Ensino médio completo.')}
+else console.log('Ensino médio incompleto');
+if (isCursandoOutraFaculdade === false)
+{console.log('Não está cursando alguma faculdade.')}
+else console.log('Está cursando uma faculdade.')
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,13 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt('Escreve aqui sua nacionalidade')
+
+if (nacionalidade === 'brasileira')
+{console.log('Nacionalidade brasileira')}
+else if (nacionalidade === 'argentina')
+{console.log('Nacionalidade argentina')}
+else if (nacionalidade === 'uruguaia')
+{console.log('Nacionalidade uruguaia')}
+else if (nacionalidade === 'chilena')
+{console.log('Nacionalidade chilena')}
+else if (nacionalidade === 'colombiana')
+{console.log('Nacionalidade colombiana')}
+else console.log('Nacionalidade não encontrada');
