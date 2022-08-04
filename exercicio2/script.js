bf6000026832e797e2491ed95b62600222b1e9ec
hilariohabhab <hilariohabhab@hotmail.com>
2022-08-03 23:16:35 -0300

    Exercicio 12

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,15 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = 31
+let terminouEnsiomedio = false
+let isCursandoOutraFaculdade = false
 
-if(){
-
+if(idade >=18){
+    console.log("Sou maior de idade")
 }else{
-	
+	console.log("Sou menor de idade")
+}
+
+if (terminouEnsiomedio) {
+    console.log("terminei o ensino médio")
+} else if (isCursandoOutraFaculdade) {
+    console.log("Não estou cursando a faculdade")
 }
