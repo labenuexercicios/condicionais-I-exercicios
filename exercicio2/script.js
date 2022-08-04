34021c90bcc578b90741d82fd22d3303908ec182
Daiane <daianelopescorrea1@gmail.com>
2022-08-04 19:33:02 -0300

    Exercicio 3

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,28 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = prompt('Qual sua idade ?');
+let isMaiorIdade = true;
+let isCursandoOutraFaculdade = true;
 
-if(){
 
+//PRIMEIRA PARTE
+if(idade >= 18){
+    console.log('A pessoa é MAIOR de idade e tem ', idade, 'anos ')
 }else{
-	
+    console.log('A pessoa é MENOR de idade e tem ', idade, 'anos ')
 }
+//SEGUNDA PARTE
+if(isMaiorIdade){
+    console.log('A pessoa terminou o ensino médio',isMaiorIdade)
+}else{
+    console.log('A pessoa NÃO terminou o ensino médio',isMaiorIdade)
+}
+
+//TERCEIRA PARTE
+
+if(isCursandoOutraFaculdade){
+    console.log('A pessoa ESTÁ cursando a faculdade', isCursandoOutraFaculdade)
+}else{
+    console.log('A pessoa NÃO está cursando a faculdade', isCursandoOutraFaculdade)
+}
+
+
+// lembrar que nem tudo deve ser pensando com base em um usario  respondendo as questoes
