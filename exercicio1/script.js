f01eec737e69def06b7255e8e95672335201003f
Daiane <daianelopescorrea1@gmail.com>
2022-08-04 18:41:47 -0300

    realização do exercicio 2

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,5 @@
-const num = //seu numero aqui
-
-if(){
-    console.log("sua mensagem aqui");
+const num = prompt('Digite um numero')
+var total = num/2;
+if((num/2 == 0)) {
+    console.log('o numero é par', num );
 }
