b3aed8989c3d1fd5b0d0fb7c9192e101e3b40e84
Jonathan-Moreira <108627997+Jonathan-Moreira@users.noreply.github.com>
2022-08-04 21:11:03 -0300

    exercicio-condicionaisI

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,24 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = 18
+let ensinoMedioConcluido = true
+let cursandoFacultada = false
 
-if(){
-
-}else{
-	
+if (idade >= 18) {
+    console.log("você é maior de idade")
 }
+
+else {
+    console.log("você é menor de idade")
+
+}
+
+if (ensinoMedioConcluido) {
+    console.log("ensino medio compelto")
+}
+
+else {
+    console.log("ensino medio incompleto")
+}
+
+if (cursandoFacultada === false) { console.log("não esta cursando faculdade") }
+
+else { console.log("cursando faculdade") }
