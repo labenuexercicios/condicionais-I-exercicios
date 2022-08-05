5f52ddb02401fa2fa186a49b33b4e0ec317c4517
Filipe Gomes <seu@email.com>
2022-08-04 21:37:35 -0300

    Barbosa-A-Filipe-Gomes-a012

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,21 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+const idade =18;
+const ensinoMedioConcluido = true;
+const naoEstaCursandoFaculdade = true;
 
-if(){
-
+if(idade >= 18){
+    console.log("A pessoa é maior de idade")
 }else{
-	
+    console.log("A pessoa é menor de idade")
+}
+
+if(ensinoMedioConcluido === true){
+    console.log("A pessoa concluiu o ensino médio")
+}else{
+    console.log("A pessoa não concluiu o ensino médio")
+}
+
+if(naoEstaCursandoFaculdade === true){
+    console.log("A pessoa está cursando outra faculdade")
+}else{
+    console.log("A pessoa não está cursando outra faculdade")
 }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,12 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt('Digite a nacionalidade')
+.toLowerCase()
+
+if(nacioanlidade === 'Brasileira' ||
+   nacionalidade ===  'Argentina' ||
+   nacionalidade === 'Uruguaia'   || 
+   nacionalidade === 'Chilena'    || 
+   nacionalidade === 'Colombiana'  ){
+     console.log('Sua nacionalidade é:', nacionalidade)
+}else{
+    console.log('Sua nacionalidade é:', nacionalidade)
+    }
