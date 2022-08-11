669a446282f360a06b59caa4ce9982968602c9cb
Ruan <ruanemidio1412@gmail.com>
2022-08-11 00:27:29 -0300

    Exercicio de condicional 1 da Labenu

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,35 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let seSim = 'sim'
 
-if(){
 
-}else{
-	
+let idade = prompt('Qual é sua idade ?');
+
+if(idade >= 18 ){
+        console.log('Você já é maior de idade.')
+        alert('Você já é maior de idade.')
+        
+}else{ 
+        console.log('Você é novinho menor!!!')
+        alert('Você é novinho mané!!!')
+        
+    }
+let isMaiorIdade = prompt('Terminou o ensino médio? Responda com: "sim" ou "não". Please!')
+    if(isMaiorIdade === seSim){
+
+    console.log(isMaiorIdade,'Parabéns coisa Linda!!!');
+    alert('Parabéns coisa Linda!!!');
+
+    }else{
+
+    console.log('VAMOS QUER ESTUDAR EM !!!')
+    alert('VAMOS QUERER ESTUDAR EM !!!')
 }
+let isCursandoOutraFaculdade = prompt('Está cursando outra faculdade? responda "sim" ou "não". Please!!!')
+   
+    if(isCursandoOutraFaculdade === seSim){
+        console.log(isCursandoOutraFaculdade,'PARABÉNS ');
+        alert('PARABÉNS, ESTUDIOSO ESSE MENINO!!!')
+    }else{
+        console.log('vamos querer estudar em');
+        alert('VAMOS QUERER ESTUDAR EM !!!')
+        
+    }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,21 @@
-let nacionalidade //= adicione o prompt aqui
+const nacionalidade = prompt('Escreva aqui sua nacionalidade, Please!');
+// if (nacionalidade === "brasileira") {
+//     console.log("Brasileira");
+//   } else if (nacionalidade === "argentina") {
+//     console.log("Argentina");
+//   } else if (nacionalidade === "uruguaia") {
+//     console.log("Uruguaia");
+//   } else if (nacionalidade === "chilena") {
+//     console.log("Chilena");
+//   } else if (nacionalidade === "colombiana") {
+//     console.log("Colombiana");
+//   } else {
+//     console.log("Nacionalidade invalida");
+//   }
+
+  if (nacionalidade === "brasileira" || nacionalidade === "argentina" || nacionalidade
+=== "uruguaia" || nacionalidade === "chilena" || nacionalidade === "colombiana") {
+    console.log(nacionalidade[0].toUpperCase() + nacionalidade.substring(1))
+} else {
+    console.log("Nacionalidade não encontrada.")
+}
