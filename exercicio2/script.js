aa9a2bdf4859e8a107546570a399b5c3a10eda4f
Guilherme <vigilato.sp@hotmail.com>
2022-08-21 18:59:14 -0300

    Atualização aula a012-condicionais1

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,21 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+const idade = 18
+const ensinoMedio = true
+const faculdade = false
 
-if(){
+if(idade >= 18){
+    console.log('A pessoa é maior de idade')
+}else {
+    console.log('A pessoa é menor de idade')
+}
 
+if(ensinoMedio){
+    console.log('Ensino médio completo')
 }else{
-	
+    console.log('Ensino médio incompleto')
+}
+
+if(!faculdade){
+    console.log('Não está cursando o ensino superior')
+}else{
+    console.log('Está cursando o ensino superior')
 }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,14 @@
-let nacionalidade //= adicione o prompt aqui
+const resposta = prompt('Escreva aqui sua nacionalidade').toLowerCase()
+
+const nacionalidade = "brasileira" || "argentina" || "uruguaia" || "chilena" || "colombiana";
+const nacionalidade2 = "brasileiro" || "argentino" || "uruguaio" || "chileno" || "colombiano";
+
+if(resposta === nacionalidade){
+    console.log(resposta)
+}
+else if(resposta === nacionalidade2){
+    console.log(resposta)
+}
+else{
+    console.log('Nacionalidade não encontrada')
+}
