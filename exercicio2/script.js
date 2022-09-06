71b622ddb500158a75e7ee8dfecfa4a903cc742b
PYTERODACTYL <pyter512@live.com>
2022-09-06 12:47:19 -0300

    feitoo

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,18 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = prompt("Qual a sua idade?")
+let isMaiorIdade=undefined
+let isCursandoOutraFaculdade=undefined  
 
-if(){
+let resposta= prompt("Voce esta cursando outra faculdade?")
 
+if(idade>=18){
+    isMaiorIdade=true
+    console.log("Eh maior de idade")
 }else{
-	
+    console.log("Eh menor de idade")
+}
+
+if(resposta.includes("sim")){
+    console.log("Esta cursando outra faculdade")
+}else{
+    console.log("Nao esta cursando outra faculdade")
 }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,15 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt("Qual a sua nacionalidade?")
+
+if(nacionalidade=="brasileiro"){
+    console.log("Voce eh brasileiro :) ")
+}else if(nacionalidade=="argentino"){
+    console.log("Voce eh argentino :) ")
+}else if(nacionalidade=="uruguaio"){
+    console.log("Voce eh uruguaio :) ")
+}else if(nacionalidade=="chileno"){
+    console.log("Voce eh chileno :) ")
+}else if(nacionalidade=="colombiano"){
+    console.log("Voce eh colombiano :) ")
+}else{
+    console.log("Nacionalidade nao encontrada")
+}
