9403f540c78dc640e0e1c671e73078b8b4289de9
karoline254 <karolineexecut@gmail.com>
2022-09-06 11:46:13 -0300

    Exercicio02

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,8 +1,8 @@
 const num = Number(prompt("Digite um número:"))
 
 if(num % 2 ===0){
-    console.log("O número é par")
+    console.log("O número é par!")
 }
 else{
-    console.log("Número ímpar")
+    console.log("Número ímpar!")
 }
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,44 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
-
-if(){
-
+let idade = Number(prompt("Qual sua idade?"))
+if(idade >= 18){
+    console.log("Maior de idade!")
 }else{
-	
+    console.log("Menor de idade!")
 }
+
+let terminouEnsinoMedio = confirm("Terminou o ensino medio?") ;
+if(terminouEnsinoMedio){
+    console.log("Ensino médio concluído!")
+}else{
+    console.log("Não terminou o ensino médio!")
+}
+
+let isCursandoOutraFaculdade = confirm("Está cursando alguma faculdade?");
+if(isCursandoOutraFaculdade){
+    console.log("Está cursando faculdade!")
+}else{
+    console.log("Não está cursando faculdade!")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if(isMaiorIdade){
+//     alert("Maior de idade!")
+// }else{
+// 	alert("Menor de idade")
+// }
+// if(idade >= 18){
+//     alert("Maior de idade!")
+// }else{
+// 	alert("Menor de idade")
+// }
