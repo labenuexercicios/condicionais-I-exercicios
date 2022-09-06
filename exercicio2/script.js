51d4ca66392ea755426666e88820b6fb416587c8
rosilenefsilva48 <rosilenefsilva48@gmail.com>
2022-09-06 16:49:15 -0300

    Exercícios resolvidos 012 clear

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,26 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+//Exercíio 2
 
-if(){
+//Escreva um código que receba três valores. Uma idade, um booleano que responda se a pessoa terminou ou não o ensino médio,
+//e um booleano que responda se a pessoa está cursando alguma faculdade.
 
-}else{
-	
+let idade = prompt("digite sua idade")
+let ensinoMedio = prompt("Você terminou o ensino médio?")
+let faculdade = prompt("Está cursando a faculdade?")
+
+if (idade >= 18) {
+    console.log("Você é maior de idade")
+} else {
+    console.log("você é menor de idade")
+}
+
+if (ensinoMedio === "sim" && idade <= 18) {
+    console.log("Você terminou o ensino médio")
+} else {
+    console.log("você não terminou o ensino médio")
+}
+
+if (faculdade === "sim" && ensinoMedio === "sim") {
+    console.log("Você está cursando faculdade")
+} else {
+    console.log("Você não está cursando faculdade")
 }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,17 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt("Escreva aqui sua nacionalidade").toLowerCase()
+
+
+if (nacionalidade === "brasileira") {
+    console.log("brasileira")
+   } else if (nacionalidade === "argentina") {
+    console.log("Argentina")
+   } else if (nacionalidade === "uruguaia") {
+    console.log("Uruguaia")
+   } else if (nacionalidade === "chilena") {
+    console.log("Chilena")
+   } else if (nacionalidade === "colombiana") {
+    console.log("colombiana")
+   } else {
+    console.log("nacionalidade não encontrada")
+}
+    
