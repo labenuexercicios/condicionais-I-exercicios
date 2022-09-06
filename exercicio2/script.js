1ced6bb9808153304ec840dbae84444062e9fc21
gustavojb1 <gustavojunqueirab@gmail.com>
2022-09-06 14:21:17 -0300

    Todos exercícios prontos

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,22 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = prompt("Digite a sua idade");
+let isTerminouEnsinoMedio = confirm("terminou o ensino médio");
+let isCursandoAlgumaFaculdade = false
 
-if(){
+if (idade >= 18) {
+  console.log("A pessoa é maior de idade");
+} else {
+  console.log("A pessoa é menor de idade");
+}
 
-}else{
-	
+if (isTerminouEnsinoMedio) {
+  console.log("A pessoa ja terminou o ensino médio");
+  isCursandoAlgumaFaculdade = confirm("Está cursando alguma faculdade?");
+} else {
+  console.log("A pessoa ainda não terminou o ensino médio");
+}
+
+if (isCursandoAlgumaFaculdade) {
+  console.log("A pessoa ja cursou a faculdade");
+} else {
+  console.log("A pessoa ainda não cursou a faculdade");
 }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,14 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt("Escreva aqui a sua nacionalidade")
+if (nacionalidade==="brasileira") {
+    console.log("sua nacionalidade é brasileira")
+} else if(nacionalidade==="argentina") {
+    console.log("sua nacionalidade é argentina")
+}else if(nacionalidade==="uruguaia") {
+    console.log("sua nacionalidade é uruguaia")
+}else if(nacionalidade==="chilena") {
+    console.log("sua nacionalidade é chilena")
+}else if(nacionalidade==="colombiana") {
+    console.log("sua nacionalidade é colombiana")
+}else{
+    console.log("sua nacionalidade não corresponde a nenhum dos valores")
+}
