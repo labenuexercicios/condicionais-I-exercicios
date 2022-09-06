8bca19217aa0f52b692b66af2cd1a67760601966
andmassbell <andremassenabelo@gmail.com>
2022-09-06 19:28:43 -0300

    a012-ex-feitos

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,21 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = Number(prompt("Qual é a sua idade?"));
+let isTerminouMedio = confirm("Você terminou o ensino médio?");
+let isCursandoOutraFaculdade = confirm("Você está cursando alguma faculdade?");
 
-if(){
+if (idade >= 18) {
+  console.log("É igual ou maior de 18 anos.");
+} else {
+  console.log("É menor que 18 anos");
+}
 
-}else{
-	
+if (isTerminouMedio) {
+  console.log("O usuario terminou o ensino médio");
+} else {
+  console.log("O usuário não terminou o ensino médio");
+}
+
+if (isCursandoOutraFaculdade) {
+  console.log("O usuario está cursando uma faculdade");
+} else {
+  console.log("O usuario não está cursando uma faculdade");
 }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,9 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt("Escreva aqui sua nacionalidade:").toLowerCase();
+
+if (nacionalidade === "brasileira" || nacionalidade === "argentina" || nacionalidade === "uruguaia" || nacionalidade === "chilena" || nacionalidade === "colombiana") {
+
+       console.log(`Sua nacionalidade é ${nacionalidade}.`);
+} else {
+
+    console.log("Nacionalidade não encontrada.")
+}
