8a7efb24bf2b1c34584d0b6084f85b78f5711d46
glauciasonda <glauciasonda@gmail.com>
2022-09-06 15:34:44 -0300

    exercicios aula 012

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,9 @@
-const num = //seu numero aqui
+const num = Number(prompt("Digite um número qualquer aqui: "))
 
-if(){
-    console.log("sua mensagem aqui");
+let resto = num % 2
+
+if(resto === 0){
+    console.log(`O número ${num}  é par`);
+} else {
+    console.log(`O número ${num}  é impar`);
 }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,18 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt("Escreva aqui a tua nacionalidade")
+nacionalidade = nacionalidade.toUpperCase()
+
+if (nacionalidade === 'BRASILEIRA' || nacionalidade === 'ARGENTINA' || nacionalidade === 'URUGUAIA' || nacionalidade === 'CHILENA' || nacionalidade === 'COLOMBIANA' ){
+    if(nacionalidade === 'BRASILEIRA'){
+        console.log(`A nacionalidade é  ${nacionalidade}` );
+    } else if (nacionalidade === 'ARGENTINA'){
+        console.log(`A nacionalidade é  ${nacionalidade}` );
+    } else if (nacionalidade === 'URUGUAIA'){
+        console.log(`A nacionalidade é  ${nacionalidade}` );
+    } else if(nacionalidade === 'CHILENA'){
+        console.log(`A nacionalidade é  ${nacionalidade}` )
+    } else if (nacionalidade === 'COLOMBIANA'){
+        console.log(`A nacionalidade é  ${nacionalidade}` )
+    }  
+} else {
+    console.log("Nacionalidade não encontrada")
+}
