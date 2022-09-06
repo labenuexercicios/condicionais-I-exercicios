75aa057bba5443a1442f6cf5ae9d78f27ea36e46
JP-Becker <jpbecker20@gmail.com>
2022-09-06 15:37:36 -0300

    AmmalB-a012-exercicios-JoaoBecker

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,48 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+//Escreva um código que receba três valores. Uma idade, um booleano que responda se a pessoa terminou ou não o ensino médio, e um booleano que responda se a pessoa está cursando alguma faculdade.
 
-if(){
+//Crie um `if` para cada variável, checando as seguintes afirmacões:
+//- Se a pessoa tem 18 anos ou mais;
+//- Se a pessoa terminou o ensino médio;
+//- Se a pessoa NÃO está cursando alguma faculdade;
 
+//Crie um `if` que imprima que a afirmacão é verdadeira, e um `else` para imprimir que a afirmacão não é verdadeira.
+
+//Caso a primeira pessoa tenha idade >=18:
+
+//<b>"A pessoa é maior de idade"</b>
+
+//e caso não seja:
+
+//<b>"A pessoa é menor de idade"</b>
+
+
+
+let idade = prompt('qual sua idade?')
+let terminouEm = prompt('vc terminou o ensino medio?')
+let isCursandoOutraFaculdade = prompt('esta cursando faculdade?')
+
+if(idade >= 18){
+    console.log("vc eh maior de idade")
 }else{
-	
+	console.log('vc é menor de idade')
+}
+
+if (terminouEm === 'nao') {
+    console.log('nao terminou o EM')
+    terminouEm = typeof(Boolean)
+    terminouEm = false
+}else {
+    console.log('terminou o EM')
+    terminouEm = typeof(Boolean)
+    terminouEm = true
+}
+
+if(isCursandoOutraFaculdade === 'sim') {
+    console.log('esta cursando faculdade')
+    terminouEm = typeof(Boolean)
+    terminouEm = true
+}else {
+    console.log('nao esta cursando faculdade')
+    terminouEm = typeof(Boolean)
+    terminouEm = false
 }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,37 @@
-let nacionalidade //= adicione o prompt aqui
+//Crie um código que receba um **prompt** que diga: "Escreva aqui sua nacionalidade" e guarde o valor da resposta em uma const `nacionalidade`.
+
+//Vamos considerar que nosso programa aceita as seguintes 5 nacionalidades:
+//- brasileira;
+//- argentina;
+//- uruguaia;
+//- chilena;
+//- colombiana;
+
+//Crie uma estrutura de `if/else if/else` que verifique se a nacionalidade guardada na const `nacionalidade` é igual a cada uma das nacionalidades acima, e caso seja, imprima a nacionalidade no console. O último `else` deve imprimir "nacionalidade não encontrada" caso o valor de `nacionalidade` não corresponda a nenhum dos valores.
+
+//> 💡 Lembre-se de prestar atenção às letras maiúsculas e minúsculas. 
+
+
+
+
+let nacionalidade = prompt('Escreva aqui sua nacionalidade').toLowerCase()
+
+const nacionalidade1 = 'brasileira'
+const nacionalidade2 = 'argentina'
+const nacionalidade3 = 'uruguaia'
+const nacionalidade4 = 'chilena'
+const nacionalidade5 = 'colombiana'
+
+if (nacionalidade === nacionalidade1) {
+    console.log('A sua nacionalidade é: ' + nacionalidade1)
+}else if (nacionalidade === nacionalidade2) {
+    console.log('A sua nacionalidade é: ' + nacionalidade2)
+}else if (nacionalidade === nacionalidade3) {
+    console.log('A sua nacionalidade é: ' + nacionalidade3)
+}else if (nacionalidade === nacionalidade4) {
+    console.log('A sua nacionalidade é: ' +nacionalidade4)
+}else if (nacionalidade === nacionalidade4) {
+    console.log('A sua nacionalidade é: ' + nacionalidade3)
+}else {
+    console.log('nacionalidade nao encontrada')
+}
