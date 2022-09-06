3c183fbc0587a28f7b3c85789ddfc23e88a378e9
JayGovinda13 <jaygovinda13@gmail.com>
2022-09-06 15:46:01 -0300

    feito

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,7 @@
-const num = //seu numero aqui
+const num = 6; //seu numero aqui
 
-if(){
-    console.log("sua mensagem aqui");
+if (num % 2 === 0) {
+  console.log("par");
+} else {
+  console.log("impar");
 }
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,21 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = Number (prompt("Quantos Anos?"));
+let terminouEnsinoMedio = confirm("Terminou o Ensino Medio?");
+let cursandoFaculdade = confirm("Cursando Faculdade?");
 
-if(){
+if (idade >= 18) {
+  console.log("Maior de idade");
+} else {
+  console.log("Menor de idade");
+}
 
-}else{
-	
+if (terminouEnsinoMedio) {
+  console.log("Ensino Medio Completo");
+} else {
+  console.log("Ensino Medio Incompleto");
+}
+
+if (cursandoFaculdade) {
+  console.log("Cursando Faculdade");
+} else {
+  console.log("Nao esta cursando faculdade");
 }
