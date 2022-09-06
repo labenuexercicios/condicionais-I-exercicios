73c80937b8d26379ced3a17ab38764d72b8fbd0c
DaniiMello <daniellemello.p@gmail.com>
2022-09-06 14:29:51 -0300

    2 exercicios feitos

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,7 @@
-const num = //seu numero aqui
+const num = prompt ("Digite um número aqui")
 
-if(){
-    console.log("sua mensagem aqui");
+if(num%2===0){
+    console.log("O número é par");
 }
+
+// Crie um código que receba uma const numérica qualquer. Crie um `if` para verificar se o número guardado na const é **par**. Caso seja, imprima no console a mensagem: "o número é par".
