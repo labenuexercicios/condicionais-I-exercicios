eb6f0454d0dba4c09f1acdf66442a1a26a851069
CamiFranca <camillaartc@gmail.com>
2022-09-06 19:25:39 -0300

    mais uma tarefa realizada

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,36 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+//usar 'confirme' para receber a resposta!
 
-if(){
 
-}else{
-	
+
+let idade= prompt('Qual a sua idade?')
+let fimEnsinoMedio = prompt('Você terminou o ensino médio?').toUpperCase
+let isCursandoFaculdade = prompt('Você não está cursando uma faculdade?').toUpperCase
+let bool1
+let bool2
+
+//transforma a resposta em valor booleano
+if(fimEnsinoMedio==="SIM"){
+    bool1=true
+}else{ bool1 = false}
+
+if(isCursandoFaculdade==="SIM"){
+    bool2=true
+}else{bool2=false}
+
+// testa a idade
+if(idade >= 18){
+    console.log('você é maior de idade.')
+}else{ console.log('você é menor de idade')
 }
+//testa ensino medio
+if(bool1 === true){
+    console.log('você terminou o ensino médio.')
+}else{ console.log('você não terminou o ensino médio.')
+  }
+//testa faculdade
+if(bool2 === true)   {
+    console.log('você não está cursando outra faculdade.')
+
+}   else {
+    console.log('Você está cursando faculdade.')
+}  
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,32 @@
-let nacionalidade //= adicione o prompt aqui
+//Pode armazenar tudo em um array
+
+const nacionalidade = ['brasileira', 'colombiana', 'uruguaia','argentina','chilena'];
+const nacionalidade2 = prompt('Qual a sua nacionalidade?').toLowerCase()
+
+if(nacionalidade.includes(nacionalidade2)){
+    console.log('Nacionalidade encontrada.')
+
+}else{
+    console.log('Nacionalidade não encontrada.')
+}
+/*const nacionalidade=prompt("Escreva aqui sua nacionalidade:")
+
+if(nacionalidade === 'brasileira'){
+    console.log('Você nasceu no Brasil.')
+}
+ else if ( nacionalidade === 'argentina'){
+console.log('Você nasceu na Argentina.')
+}
+else if ( nacionalidade === 'colombiana'){
+    console.log('Você nasceu na Colômbia.')
+}
+else if (nacionalidade === 'uruguaia'){
+    console.log('Você nasceu no Uruguai.')
+}
+else if (nacionalidade === 'chilena'){
+    console.log('Você nasceu no Chile.')
+}
+else{
+    console.log('Nacionalidade não encontrada.')
+}*/
+
