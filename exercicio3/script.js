3544dd3fb9579368722ba4d00733669bf81919b9
karoline254 <karolineexecut@gmail.com>
2022-09-06 11:58:51 -0300

    Exercicio03

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,15 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt("Digite sua nacionalidade?")
+
+if(nacionalidade === "brasileira"){
+    console.log("Nacionalidade aceita!", nacionalidade)
+}else if(nacionalidade === "argentina"){
+    console.log("Nacionalidade aceita!", nacionalidade)
+}else if(nacionalidade === "uruguaia"){
+    console.log("Nacionalidade aceita!", nacionalidade)
+}else if(nacionalidade === "chilena"){
+    console.log("Nacionalidade aceita: ", nacionalidade)
+}else if(nacionalidade === "chilena"){
+    console.log("Nacionalidade aceita: ", nacionalidade)
+}else{
+    console.log("Nacionalidade não aceita!")
+}
