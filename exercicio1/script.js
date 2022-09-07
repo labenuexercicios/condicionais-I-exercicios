45e772ca82ee4f0ed6cdd33392583147df5fe378
dorotrodrigues <dorotbarbosa57@gmail.com>
2022-09-07 18:14:04 -0300

    Exercício concluído

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,12 @@
-const num = //seu numero aqui
+// # Exercício 1
 
-if(){
-    console.log("sua mensagem aqui");
+// Crie um código que receba uma const numérica qualquer. Crie um `if` para verificar se o número guardado na const é **par**. Caso seja, imprima no console a mensagem: "o número é par".
+
+const num = prompt("Digite um número:")
+const resto = num % 2
+
+if(resto === 0) {
+    console.log("O número é par.")
+} else {
+    console.log("Número ímpar.")
 }
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,46 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+// # Exercício 2
 
-if(){
+// Escreva um código que receba três valores. Uma idade, um booleano que responda se a pessoa terminou ou não o ensino médio, e um booleano que responda se a pessoa está cursando alguma faculdade.
 
-}else{
-	
+// Crie um `if` para cada variável, checando as seguintes afirmacões:
+// - Se a pessoa tem 18 anos ou mais;
+// - Se a pessoa terminou o ensino médio;
+// - Se a pessoa NÃO está cursando alguma faculdade;
+
+// Crie um `if` que imprima que a afirmacão é verdadeira, e um `else` para imprimir que a afirmacão não é verdadeira.
+
+// <details>
+
+// <summary>Exemplo</summary>
+
+// Caso a primeira pessoa tenha idade >=18:
+
+// <b>"A pessoa é maior de idade"</b>
+
+// e caso não seja:
+
+// <b>"A pessoa é menor de idade"</b>
+
+// </details>
+
+let idade = prompt("Qual a sua idade?")
+let ensinoMedio = Boolean(prompt("Você concluiu o ensino médio?"))
+let faculdade = Boolean(prompt("Está cursando faculdade?"))
+
+if(idade >= 18){
+    console.log("Maior de idade.")
+}else if(idade <18){
+    console.log("Menor de idade.")
+}
+
+if(ensinoMedio){
+    console.log("Concluiu o ensino médio.")
+} else {
+    console.log("Não concluiu o ensino médio.")
+}
+
+if(faculdade){
+    console.log("Estudante de faculdade.")
+} else {
+    console.log("Não faz faculdade.")
 }
