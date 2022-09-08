361c90c966b256c11dbbf75a98e860029b4e5564
Jessica <jessica.thamiresnunes@gmail.com>
2022-09-08 19:17:31 -0300

    Atividade 012 realizada com sucesso

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,8 @@
-const num = //seu numero aqui
+const num = (prompt ("Digite qualquer número:"))
+let parOuImpar = (num%2)
 
-if(){
-    console.log("sua mensagem aqui");
+if(parOuImpar===0){
+    console.log("O número é par!!!");
+} else if (parOuImpar===1){
+    console.log("O número e ímpar!!!")
 }
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,24 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = prompt("Digite sua idade:")
+let isEnsinoMedio = prompt("Responda com SIM ou NÃO, você terminou o ensino médio?")
+let isCursandoOutraFaculdade= prompt("Responda com SIM ou NÃO, você está cursando alguma faculdade?")
 
-if(){
+if(idade>=18){
+    console.log ("Você é maior de 18!")
+}else{ 
+    console.log("Você ainda não atingiu a maior idade!")
+	
+}
 
-}else{
+if(isEnsinoMedio==="NÃO"){
+    console.log ("Você não terminou o ensino médio!")
+}else{ 
+    console.log("Você terminou o ensino médio!")
+	
+}
+
+if(isCursandoOutraFaculdade==="SIM"){
+    console.log ("Você está cursando faculdade!")
+}else{ 
+    console.log("Você não está cursando faculdade!")
 	
 }
