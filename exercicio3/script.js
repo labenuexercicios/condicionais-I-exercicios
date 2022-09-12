fbcd988ddf8f94651401d37d27c0950bafd32384
Carlos Bergson <104369215+cblouzada@users.noreply.github.com>
2022-09-11 21:31:32 -0300

    Update script.js

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,19 @@
-let nacionalidade //= adicione o prompt aqui
+// Usando prompt dgite sua nacionalidade.
+
+const nacionalidade = prompt("Escreva aqui sua nacionalidade")
+
+if(nacionalidade === "brasileira"){
+    console.log("a pessoa é do Brasil!");
+} else if(nacionalidade === "argentina"){
+    console.log("a pessoa é da Argentina!");
+} else if(nacionalidade === "uruguaia"){
+    console.log("a pessoa é do Uruguai!");
+} else if(nacionalidade === "chilena"){
+    console.log("a pessoa é do Chile!");
+} else if(nacionalidade === "colombiana"){
+    console.log("a pessoa é da Colômbia!");
+} else{
+    console.log("nacionalidade não encontrada")
+}
+
+// Caso não seja encontrando sua nacionalidade, a mensagem nacionalidade não encontrada sera exibida.
