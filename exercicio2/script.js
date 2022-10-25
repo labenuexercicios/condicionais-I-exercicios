c501926dc19e5171acb16efe0b443f5b3db2bb01
Jessica Faustino <jessicacfaustino@gmail.com>
2022-10-25 11:25:01 -0300

    Subindo repositório

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,19 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = prompt("Idade: ");
+let isMaiorIdade = confirm("Terminou o ensino médio?");
+let isCursandoOutraFaculdade = confirm("Está cursando alguma faculdade?");
 
-if(){
-
+if(idade => 18){
+    console.log ("A pessoa é maior de idade!")
 }else{
-	
+    console.log ("A pessoa não é maior de idade!")
+}
+if(isMaiorIdade){
+    console.log ("Concluiu o ensino médio!")
+} else {
+    console.log("Não concluiu o ensino médio!")
+}
+if(isCursandoOutraFaculdade===false){
+    console.log("Não está cursando nenhuma faculdade!")
+} else{
+    console.log ("Está cursando faculdade!");
 }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,14 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt("Qual sua nacioncionalidade?").toLowerCase();
+
+if (nacionalidade == 'brasileira'||nacionalidade == 'brasileiro') {
+    console.log(`Você é ${nacionalidade}`);
+} else if (nacionalidade === 'uruguaia' || nacionalidade === 'uruguaio'){
+    console.log(`Você é ${nacionalidade}`);
+} else if (nacionalidade==='chilena' || nacionalidade === 'chileno'){
+    console.log (`Você é ${nacionalidade}`);
+} else if (nacionalidade === 'colombiana' || nacionalidade === 'colombiano'){
+    console.log (`Você é ${nacionalidade}`);
+}else{
+    alert("Nacionalidade não encontrada");
+}
+
