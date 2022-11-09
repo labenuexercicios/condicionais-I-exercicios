85baa8d9a1857929ffb830afac3b95043d440035
Alex Fernandes da Silva <alex.rgracie@gmail.com>
2022-11-09 20:48:05 -0300

    Exercicio respondido

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,7 @@
-const num = //seu numero aqui
+const num = 2//seu numero aqui
 
-if(){
-    console.log("sua mensagem aqui");
+if(num%2 == 0){
+    console.log("Par");
+} else {
+    console.log("Impar")
 }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,15 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt("Digite sua nacionalidade")
+
+if(nacionalidade == 'brasileira') {
+    console.log('brasileira')
+} else if (nacionalidade == 'argentina') {
+    console.log('argentina')
+} else if (nacionalidade == 'uruguaia') {
+    console.log('uruguaia')
+} else if (nacionalidade == 'chilena') {
+    console.log('chilena')
+} else if (nacionalidade == 'colombiana') {
+    console.log('colombiana')
+} else {
+    console.log("nacionalidade não encontrada")
+}
