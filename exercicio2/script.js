dcf60a8875b73444b995e1915194e54755a892a2
RocioSuarez <suarez.rocio4344@gmail.com>
2022-12-01 17:03:33 -0300

    first commit

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,38 @@
-let idade;
-let isTerminouEnsinoMedio;
-let isCursandoOutraFaculdade;
+/**Exercício 2
 
-if(){
+Escreva uma função que receba três valores. Uma idade, um booleano que 
+responda se a pessoa terminou ou não o ensino médio, e um booleano que 
+responda se a pessoa está cursando alguma faculdade.
 
-}else{
-	
+Crie um if para cada variável, checando as seguintes afirmacões:
+
+    Se a pessoa tem 18 anos ou mais;
+    Se a pessoa terminou o ensino médio;
+    Se a pessoa NÃO está cursando alguma faculdade;
+
+Crie um if que imprima que a afirmacão é verdadeira, e um else para imprimir
+que a afirmacão não é verdadeira.
+Exemplo */
+const idade = Number(prompt("quantos anos você tem?"));
+const ensinoMedio = confirm("você terminou o ensino medio?"); 
+const faculdade = confirm("você terminou a faculdade?");
+
+
+function academica (idade, ensinoMedio, faculdade){
+    if(idade >= 18){
+        console.log("voce si teim 18 anos")
+    }else if(idade <= 18){
+        console.log("voce ao tem 18 anos")
+    }else if(ensinoMedio == true /*|| ensinoMedio == sim*/){
+        console.log("voce terminou ensino Medio!")
+    }else if(ensinoMedio == false /*|| ensinoMedio == nao*/){
+        console.log("voce nao termino ensino Medio!")   
+    }else if(faculdade == false /*|| faculdade == nao*/){
+        console.log("voce nao esta cursando ninguma falculdade")
+    }else{
+        console.log("voce nao tem 18 anos, no terminou ensino medio y nao esta cursando nunguma faculdade!")
+    }  
 }
+
+academica(idade, ensinoMedio, faculdade)
+ 
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,32 @@
-let nacionalidade //= adicione o prompt aqui
+/**Exercício 3
+Crie uma função que receba do usuário sua nacionalidade, 
+utilize um prompt que diga: "Escreva aqui sua nacionalidade" 
+e guarde o valor da resposta em uma const nacionalidade.
+
+Vamos considerar que nosso programa aceita as seguintes 5 nacionalidades:
+
+    brasileira;
+    argentina;
+    uruguaia;
+    chilena;
+    colombiana;
+
+Crie uma estrutura de if/else if/else que verifique se a nacionalidade 
+guardada na const nacionalidade é igual a cada uma das nacionalidades acima, 
+e caso seja, imprima a nacionalidade no console. O último else deve imprimir 
+"nacionalidade não encontrada" caso o valor de nacionalidade não corresponda 
+a nenhum dos valores.
+
+    💡 Lembre-se de prestar atenção às letras maiúsculas e minúsculas.
+ */
+
+    const nacionalidade = prompt("qual es sua nacionalidade?")
+
+    function asignaNacionalidade(nacionalidade){
+    if(nacionalidade ===  brasileira || nacionalidade === argentina || nacionalidade === uruguaia || nacionalidade === chilena || nacionalidade === colombiana){
+    console.log(`Sua nacionalidade e: ${nacionalidade}`)
+    }else{
+    console.log("nacionalidade não encontrada");
+    }
+    }
+    asignaNacionalidade(nacionalidade)
