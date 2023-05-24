6e2e9dbcc1d8d71f4be87cbaa0b174e37dbb4aba
Camila Lopes <camilalopesqta@gmail.com>
2023-05-24 18:07:18 -0300

    exercícios resolvidos condicionais I

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,21 +1,6 @@
-//Crie uma função que recebe do usuário sua nacionalidade, 
-//utilizando um **prompt** com a seguinte mensagem: 
-//"Escreva aqui sua nacionalidade." 
-//e guarde o valor da resposta em uma const `nacionalidade`.
-
 const nacionalidade = prompt("Escreva aqui sua nacionalidade.").toLowerCase()
+
 function nacionalidade1 (){
-//Vamos considerar que nosso programa aceita as seguintes 5 nacionalidades:
-//- brasileira;
-//- argentina;
-//- uruguaia;
-//- chilena;
-//- colombiana;
-//Crie uma estrutura de `if/else if/else` que verifique se a nacionalidade 
-//guardada na const `nacionalidade` é igual a cada uma das nacionalidades acima, 
-//e caso seja, imprima a nacionalidade no console. O último `else` deve imprimir 
-//"nacionalidade não encontrada" caso o valor de `nacionalidade` não corresponda 
-//a nenhuma das possibilidades acima.
 if (nacionalidade === "brasileira"){
     console.log("brasileira")
 }else if (nacionalidade === "argentina"){
